09d13d7668181af0d04f9da47046a5f42bfea11d
Scott Williams <tocsoft@gmail.com>
2017-01-21 16:16:30 +0000

    image sharp example app

---
+++ ImageSharpRenderSample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ImageSharp;
+using ImageSharp.Drawing.Brushes;
+using ImageSharp.Drawing;
+using System.IO;
+
+namespace ImageSharpRenderSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var img = new Image<Color>(100, 100);
+
+            var brush = new SolidBrush(Color.HotPink);
+
+            var rectangle = new Shaper2D.Rectangle(10, 10, 40, 40);
+            img.BackgroundColor(Color.BlueViolet);
+
+            img.Apply(new FillShapeProcessor<Color>(brush, rectangle, GraphicsOptions.Default));
+
+            using (var fs = File.OpenWrite("test.bmp"))
+            {
+                img.SaveAsBmp(fs);
+            }
+        }
+    }
+}
